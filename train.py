9e037c32e5e2609f73a27999340b9bca7671159f
sxtang2023 <67324977+tansaox2008123@users.noreply.github.com>
2025-04-09 19:02:06 +0800

    Update and rename train_with_guidance.py to train.py

--- train.py
+++ train.py
@@ -206,55 +206,6 @@ def rna_seq_embbding(OriginSeq, batch_converter, EmbeddingModel, device):
     return token_embeddings
 
 
-def read_data_without_LLM(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-
-    rnas = []
-    input_seqs = []
-    true_seqs = []
-    bd_scores = []
-
-    for line in lines:
-        words = line.split()
-        b = words[-1]
-        rna_one_hot = convert_to_rna_sequence_rna_fm(b)
-
-        values_list = list(rna_one_hot)
-
-        values_list.insert(0, 0)
-
-        input_seq = values_list[:-1]
-        true_seq = values_list[1:]
-
-        decimal_part = float(words[0])
-        decimal_part = (sigmoid(decimal_part, 0.05) - 0.5) * 2.0
-
-        rna_fm = values_list[1:]
-
-        rnas.append(rna_fm)
-        input_seqs.append(input_seq)
-        true_seqs.append(true_seq)
-        bd_scores.append(decimal_part)
-    return rnas, input_seqs, true_seqs, bd_scores
-
-
-def get_data_without_llm(file_path, is_batch, device):
-    rnas, input_seqs, true_seqs, bd_scores = read_data_without_LLM(file_path)
-
-    if is_batch:
-        rnas1 = torch.tensor(rnas, dtype=torch.float32)
-        input_seqs1 = torch.tensor(np.asarray(input_seqs))
-        true_seqs1 = torch.tensor(np.asarray(true_seqs))
-        bd_scores1 = torch.tensor(np.asarray(bd_scores))
-    else:
-        rnas1 = torch.tensor(rnas).to(device)
-        input_seqs1 = torch.tensor(np.asarray(input_seqs)).to(device)
-        true_seqs1 = torch.tensor(np.asarray(true_seqs)).to(device)
-        bd_scores1 = torch.tensor(np.asarray(bd_scores)).to(device)
-
-    return rnas1, input_seqs1, true_seqs1, bd_scores1
-
 
 def train_guidance_LLM_rna_fm(train_file, test_file, batch_size, model_name, device):
     tr_feats, tr_input_seqs, tr_true_seqs, tr_bd_scores = get_data_rna_fm(train_file, is_batch=True, device=device)
@@ -532,141 +483,6 @@ def train_guidance_LLM_Evo(train_file, test_file, batch_size, model_name, device
     torch.save(model, 'model/' + model_name + '.model')
 
 
-def train_guidance_without_LLM(train_file, test_file, batch_size, model_name, device):
-    tr_feats, tr_input_seqs, tr_true_seqs, tr_bd_scores = get_data_without_llm(train_file, is_batch=True, device=device)
-    te_feats, te_input_seqs, te_true_seqs, te_bd_scores = get_data_without_llm(test_file, is_batch=True, device=device)
-
-    train_data = torch_data.TensorDataset(tr_feats, tr_input_seqs, tr_true_seqs, tr_bd_scores)
-    test_data = torch_data.TensorDataset(te_feats, te_input_seqs, te_true_seqs, te_bd_scores)
-
-    train_loader = DataLoader(dataset=train_data,
-                              batch_size=batch_size,
-                              shuffle=True,
-                              num_workers=0,
-                              pin_memory=True,
-                              drop_last=False)
-    test_loader = DataLoader(dataset=test_data,
-                             batch_size=batch_size,
-                             shuffle=True,
-                             num_workers=0,
-                             pin_memory=True,
-                             drop_last=False)
-
-    model = FullModel_guidance_Without_LLM(input_dim=20,
-                                           model_dim=128,
-                                           tgt_size=5,
-                                           n_declayers=2,
-                                           d_ff=128,
-                                           d_k_v=64,
-                                           n_heads=2,
-                                           dropout=0.05)
-
-    model = model.to(device)
-
-    loss_func1 = nn.MSELoss()
-    loss_func2 = nn.CrossEntropyLoss(ignore_index=0)
-
-    w = 0.15
-    fw = open('log/' + model_name + '_training_log.txt', 'w')
-
-    for epoch in range(250):
-        start_t = time.time()
-        loss1_value = 0.0
-        loss2_value = 0.0
-        acc2 = 0.0
-        b_num = 0.0
-
-        # optimizer = torch.optim.Adam(model.parameters(), lr=(250.0 - epoch) * 0.00001)
-        # optimizer = torch.optim.Adam(model.parameters(), lr=max((250.0 - epoch) * 0.00001, 0.0001))
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.0006)
-
-        model.train()
-        for i, data in enumerate(train_loader):
-            inputs, input_seqs, true_seqs, labels = data
-            inputs = inputs.to(device)
-            input_seqs = input_seqs.to(device)
-            true_seqs = true_seqs.to(device)
-            labels = labels.to(device)
-
-            labels = labels.float().view(-1, 1)
-            bind_socres, pred_seqs = model(inputs, input_seqs)
-
-            pred_seqs = torch.softmax(pred_seqs, -1)
-            true_seqs = true_seqs.view(-1)
-            pred_seqs = pred_seqs.view(true_seqs.shape[0], 5)
-
-            loss1 = loss_func1(bind_socres, labels)
-            loss2 = loss_func2(pred_seqs, true_seqs)
-            pred_seqs = torch.argmax(pred_seqs, -1)
-            acc2 += torchmetrics.functional.accuracy(pred_seqs,
-                                                     true_seqs,
-                                                     task="multiclass",
-                                                     num_classes=5,
-                                                     ignore_index=0,
-                                                     average="micro")
-
-            loss = w * loss1 + (1.0 - w) * loss2
-            loss1_value += loss1.item()
-            loss2_value += loss2.item()
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            b_num += 1.
-
-        test_loss1_value = 0.0
-        test_loss2_value = 0.0
-
-        te_acc2 = 0.0
-        test_b_num = 0.
-
-        model.eval()
-        for i, data in enumerate(test_loader):
-            inputs, input_seqs, true_seqs, labels = data
-            inputs = inputs.to(device)
-            input_seqs = input_seqs.to(device)
-            true_seqs = true_seqs.to(device)
-            labels = labels.to(device)
-
-            bind_socres, pred_seqs = model(inputs, input_seqs)
-            pred_seqs = torch.softmax(pred_seqs, -1)
-            labels = labels.float().view(-1, 1)
-
-            true_seqs = true_seqs.view(-1)
-            pred_seqs = pred_seqs.view(true_seqs.shape[0], 5)
-
-            loss1 = loss_func1(bind_socres, labels)
-            loss2 = loss_func2(pred_seqs, true_seqs)
-
-            pred_seqs = torch.argmax(pred_seqs, -1)
-            te_acc2 += torchmetrics.functional.accuracy(pred_seqs,
-                                                        true_seqs,
-                                                        task="multiclass",
-                                                        num_classes=5,
-                                                        ignore_index=0,
-                                                        average="micro")
-
-            test_loss1_value += loss1.item()
-            test_loss2_value += loss2.item()
-            test_b_num += 1.
-        end_t = time.time()
-        fw.write('{:4d}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\n'.format(epoch,
-                                                                  loss1_value / b_num,
-                                                                  loss2_value / b_num,
-                                                                  test_loss1_value / test_b_num,
-                                                                  test_loss2_value / test_b_num,
-                                                                  acc2 / b_num,
-                                                                  te_acc2 / test_b_num), )
-        print('Epoch:', '%04d' % (epoch + 1),
-              '| tr_loss1 =', '{:.4f}'.format(loss1_value / b_num),
-              '| tr_loss2 =', '{:.4f}'.format(loss2_value / b_num),
-              '| tr_acc =', '{:.2f}'.format(acc2 / b_num),
-              '| te_loss1 =', '{:.4f}'.format(test_loss1_value / test_b_num),
-              '| te_loss2 =', '{:.4f}'.format(test_loss2_value / test_b_num),
-              '| te_acc =', '{:.2f}'.format(te_acc2 / test_b_num),
-              '| time =', '{:.2f}'.format(end_t - start_t)
-              )
-    torch.save(model, 'model/' + model_name + '.model')
-
 
 def main():
     parser = argparse.ArgumentParser(description="Choose which function to run.")
@@ -698,8 +514,6 @@ def main():
         train_guidance_LLM_rna_fm(train_file, test_file, batch_size, model_name, device)
     elif args.function == '2':
         train_guidance_LLM_Evo(train_file, test_file, batch_size, model_name, device)
-    elif args.function == '3':
-        train_guidance_without_LLM(train_file, test_file, batch_size, model_name, device)
 
 
 if __name__ == '__main__':
