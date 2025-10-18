73156eedd8b36166d6824d2582dd1d4fca44ccce
sxtang2023 <67324977+tansaox2008123@users.noreply.github.com>
2025-10-19 02:40:11 +0800

    Update train_Ernie.py

--- other_LM/train_Ernie.py
+++ other_LM/train_Ernie.py
@@ -4,7 +4,7 @@ from torch.utils.data import Dataset, DataLoader
 import torchmetrics
 import time
 import argparse
-from model_zh import *
+from model import *
 from multimolecule import RnaTokenizer, RnaErnieModel
 from torchmetrics.functional import pearson_corrcoef
 import os
@@ -309,3 +309,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
